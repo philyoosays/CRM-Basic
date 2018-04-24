23932b572d676c4efb2c0111fbbf2a8d6e260070
Phil Yoo <philyoo@Phils-MacBook-Air.local>
2018-04-24 00:40:34 -0400

    tweak

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.set('database', process.env.DATABASE)
+
 const pgp = require('pg-promise')({
   query: q => console.log(q.query),
 });
+
 const config = {
   host: 'localhost',
   port: 5432,
-  database: 'crm_unit02'
+  database: app.get('database')
 };
+
 const db = pgp(config);
 
 module.exports = db;
